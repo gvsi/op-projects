1ac02490593cfe4b6ef1fa17f6f52c389374a5e4
Giovanni Alcantara <me@giovannialcantara.com>
2015-01-14 11:05:04 +0000

    Completed Week1 tutorial

---
+++ week1/Adder.java
@@ -0,0 +1,7 @@
+public class Adder {
+	public static void main (String[] args) {
+		int myNumber = Integer.parseInt(args[0]);
+		int myNumber2 = Integer.parseInt(args[1]);
+		System.out.println(myNumber + myNumber2);
+	}
+}
---
+++ week1/Multiplier.java
@@ -0,0 +1,5 @@
+public class Multiplier {
+	public static void main(String args[]) {
+		System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[1]) * Integer.parseInt(args[2]));
+	}
+}
